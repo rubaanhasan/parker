34c36e8162e6fb91599c06c9d1169378dc88ef46
SudhST <145555381+SudhST@users.noreply.github.com>
2023-11-03 17:57:16 +0530

    Added Afterlogin & JS

---
+++ JS/afterlogin.js
@@ -0,0 +1,14 @@
+let elem=document.getElementById("Start")
+let yourDate = new Date().toLocaleDateString('en-CA')
+// console.log(yourDate)
+elem.setAttribute("value",yourDate)
+elem.setAttribute("min",yourDate)
+elem.setAttribute("max",yourDate)
+
+
+Mall Name : 
+In Time : 
+Out Time : 
+Date : 
+Number Plate : 
+Price :  
