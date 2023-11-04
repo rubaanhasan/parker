d839a83697f2181ff01c8a926c45e4653ff2ce30
SudhST <145555381+SudhST@users.noreply.github.com>
2023-11-04 13:56:54 +0530

    Add files via upload
    converted to ejs

--- JS/afterlogin.js
+++ JS/afterlogin.js
@@ -1,23 +1,37 @@
-let elem=document.getElementById("Start1")
-let yourDate = new Date().toLocaleDateString('en-CA')
-elem.setAttribute("value",yourDate)
-elem.setAttribute("min",yourDate)
-elem.setAttribute("max",yourDate)
+// let elem=document.getElementById("Start1")
+// let yourDate = new Date().toLocaleDateString('en-CA')
+// elem.setAttribute("value",yourDate)
+// elem.setAttribute("min",yourDate)
+// elem.setAttribute("max",yourDate)
 
-elem=document.getElementById("Start2")
-yourDate = new Date().toLocaleDateString('en-CA')
-elem.setAttribute("value",yourDate)
-elem.setAttribute("min",yourDate)
-elem.setAttribute("max",yourDate)
+// elem=document.getElementById("Start2")
+// yourDate = new Date().toLocaleDateString('en-CA')
+// elem.setAttribute("value",yourDate)
+// elem.setAttribute("min",yourDate)
+// elem.setAttribute("max",yourDate)
 
-elem=document.getElementById("Start3")
-yourDate = new Date().toLocaleDateString('en-CA')
-elem.setAttribute("value",yourDate)
-elem.setAttribute("min",yourDate)
-elem.setAttribute("max",yourDate)
+// elem=document.getElementById("Start3")
+// yourDate = new Date().toLocaleDateString('en-CA')
+// elem.setAttribute("value",yourDate)
+// elem.setAttribute("min",yourDate)
+// elem.setAttribute("max",yourDate)
 
-elem=document.getElementById("Start4")
-yourDate = new Date().toLocaleDateString('en-CA')
-elem.setAttribute("value",yourDate)
-elem.setAttribute("min",yourDate)
-elem.setAttribute("max",yourDate)
+// elem=document.getElementById("Start4")
+// yourDate = new Date().toLocaleDateString('en-CA')
+// elem.setAttribute("value",yourDate)
+// elem.setAttribute("min",yourDate)
+// elem.setAttribute("max",yourDate)
+
+
+document.getElementById("Form5").addEventListener('click',function(e){
+    document.getElementById("SubmitForm").click();
+})
+document.getElementById("Form6").addEventListener('click',function(e){
+    document.getElementById("SubmitForm").click();
+})
+document.getElementById("Form7").addEventListener('click',function(e){
+    document.getElementById("SubmitForm").click();
+})
+document.getElementById("Form8").addEventListener('click',function(e){
+    document.getElementById("SubmitForm").click();
+})
