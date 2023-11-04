f89d57b3b29db80e49e26488caf45e45f6fe03d4
busybrowsensei1 <darshan_rb@cs.iitr.ac.in>
2023-11-04 17:11:02 +0530

    backend update js file

--- frontend/JS/user.js
+++ frontend/JS/user.js
@@ -1,27 +1,27 @@
-fetch("/JS/user2.json")
-    .then(function(response){
-        if (!response.ok) {
-            throw new Error('Network response was not ok');
-        }
-        return response.json();
-    })
-    .then(function(products){
-        let placeholder = document.querySelector("#data-output2");
-        let out = "";
-        for(let product of products){
-            out += `
+fetch("user2.json")
+  .then(function (response) {
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+    return response.json();
+  })
+  .then(function (products) {
+    let placeholder = document.querySelector("#data-output2");
+    let out = "";
+    for (let product of products) {
+      out += `
             <tr>
             <td>${product.numberplate}</td>
-            <td>${product.in-time}</td>
-            <td>${product.out-time}</td>
+            <td>${product.in - time}</td>
+            <td>${product.out - time}</td>
             <td>${product.date}</td>
             <td>${product.status}</td>
             </tr>
             `;
-        }
+    }
 
-        placeholder.innerHTML = out;
-    })
-    .catch(function(error) {
-        console.error('There was a problem with the fetch operation:', error);
-    });
+    placeholder.innerHTML = out;
+  })
+  .catch(function (error) {
+    console.error("There was a problem with the fetch operation:", error);
+  });
