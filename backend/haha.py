ddfcef635e0eac99dc634a336e40796819141b2f
Rubaan Hasan <121686140+rubaanhasan@users.noreply.github.com>
2023-11-08 00:05:23 +0530

    Delete backend/haha.py

--- backend/haha.py
+++
@@ -1,9 +0,0 @@
-file_name = "/ai_files/test.txt"
-
-# Open the file in write mode to overwrite it or create a new file
-with open(file_name, "w") as file:
-    file.write("MH15XX00234\n")
-    file.write("camera2\n")
-    file.write("06:55:10 ")
-
-print(f"Data has been written to '{file_name}'.")
